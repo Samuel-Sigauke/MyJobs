7ebaedd833471022fc3f7c0b085b1db8f6c60efc
unknown <samuels@digitalTrans5.stewardbank.co.zw>
2017-11-29 10:23:20 +0200

    Job2 commited

---
+++ Job2.java
@@ -0,0 +1,16 @@
+public class Job2{
+
+	public static void main (String []args){
+	
+		for(int i=1;i<11;i++) {
+		
+			System.out.println("Process"+" "+i +" "+"executing...");
+		
+		}
+	
+	
+	}
+
+
+
+}
