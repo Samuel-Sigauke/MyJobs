e09d34f1465fe5ecd5da80e86aa62269bdb684f0
unknown <samuels@digitalTrans5.stewardbank.co.zw>
2018-01-19 10:29:12 +0200

    edited jenkinsjob.jav

--- JenkinsJob.java
+++ JenkinsJob.java
@@ -3,6 +3,8 @@
 		public static void main (String [] args){
 		
 		System.out.println("Welcome to Jenkins.......... ENJOY!!!!!!");
+		System.out.println("Welcome to Jenkins.......... ENJOY!!!!!!");
+		
 		
 		}
 	
